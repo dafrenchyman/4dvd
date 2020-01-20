f80fb14c9cf73bc002b41a5aad7d404d7fcef605
Isaiah Dorado <isaiahdorado@gmail.com>
2020-01-19 17:50:08 -0800

    ID - Adding interactivity with main page strings

--- src/app/view.component.ts
+++ src/app/view.component.ts
@@ -38,6 +38,7 @@ declare var jQuery: any;
   // providers: [GetJson]
 })
 export class ViewComponent implements OnInit, AfterViewInit {
+  selectedIndex = 0;
   datasetTitle = "Change the current dataset";
   timeTitle = "Change the year and month";
   graphicTitle = "Change visual settings";
@@ -55,6 +56,8 @@ export class ViewComponent implements OnInit, AfterViewInit {
   riverDesc = "Toggles rendered quality of rivers";
   lakeDesc = "Toggles rendered quality of lakes";
   displayLoc = "right";
+  layerDateTT = "Click to change the date";
+  titleToolTip = "Click to change data set";
   exitsidenav = "Closes the settings tab";
   timeSeriesVal: any;
   @ViewChild("ClimateGl") private _canvas: ElementRef;
