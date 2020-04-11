7a5b907b7dfe4a89c042733d9316a9ae6e2dc535
Isaiah Dorado <isaiahdorado@gmail.com>
2020-04-10 18:14:55 -0700

    ID - More modifications to csv file

--- src/app/view.component.ts
+++ src/app/view.component.ts
@@ -547,27 +547,27 @@ export class ViewComponent implements OnInit, AfterViewInit {
     );
     const levelUnits = this._model.settings.LevelName.split(" ")[1];
     let dataUnits;
-    valueTitle === "Air Temperature" || valueTitle === "Soil Temperature"
+    valueTitle === "Air Temperature" || valueTitle === "Soil Temperature" // Air Temp DataUnits are degK, Soil Temp does not have a DataUnits value
       ? (dataUnits = "degC")
       : (dataUnits = this._model.settings.DataUnits);
+    let level = this._model.settings.LevelName;
 
-    // Create the header
-    csvContent += `Latitude,Longitude,${valueTitle} [${dataUnits}],Level [${
-      this._model.settings.LevelName.split(" ")[1]
-    }],Date\n`;
+    // Create Header
+    if (level === "Default") { // Default = Dataset with single level
+      level = "Single Level";
+      csvContent += `Latitude,Longitude,${valueTitle} [${dataUnits}],Level,Date\n`;
+    } else {
+      csvContent += `Latitude,Longitude,${valueTitle} [${dataUnits}],Level [${level.split(" ")[1]}],Date\n`;
+      level = level.split(" ")[0];
+    }
+
     const rawData = this._model._world.GetRawData();
     let dataString;
     if (rawData.Lat.length > 0) {
       for (let i = 0; i < rawData.Lat.length; i++) {
-        i === 0
-          ? (dataString = `${rawData.Lat[i]},${
-              rawData.Lon[i]
-            },${rawData.ValueFinal[i].toFixed(3)},${
-              this._model.settings.LevelName.split(" ")[0]
-            },${this._model.settings.CurrDate.substring(0, 7)}`)
-          : (dataString = `${rawData.Lat[i]},${
-              rawData.Lon[i]
-            },${rawData.ValueFinal[i].toFixed(3)}`);
+        i === 0 ? // Create initial subheader line that includes non repeating values
+          (dataString = `${rawData.Lat[i]},${rawData.Lon[i]},${rawData.ValueFinal[i].toFixed(3)},${level},${this._model.settings.CurrDate.substring(0, 7)}`) :
+          (dataString = `${rawData.Lat[i]},${rawData.Lon[i]},${rawData.ValueFinal[i].toFixed(3)}`);
         csvContent += dataString + "\n";
       }
 
@@ -581,7 +581,7 @@ export class ViewComponent implements OnInit, AfterViewInit {
           this._model.settings.Dataset.DatabaseStore.length
         ) +
           "_GridData_" +
-          this._model.settings.CurrDate +
+          this._model.settings.CurrDate.substr(0,7) +
           ".csv"
       );
       link.click();
