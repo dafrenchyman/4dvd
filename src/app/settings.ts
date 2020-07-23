a9b74a8f212c068764e1969aa106218ebb8e07af
snehaldhadge <snehaldhadge@gmail.com>
2020-07-23 15:45:18 -0700

    SI: Added Menu on TimeSeries dialog
    Added Save graph and Download data as Menu option in Time Series Dialog
    
    Added Data Title to Time Series with Latitude and Longitude in title

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -172,8 +172,9 @@ export class Settings {
     if (str === "") {
       return " ";
     }
-    // Remove any text in opening and closing bracket in the Title like (10mb-100mb)
+    //Remove any text in opening and closing bracket in the Title like (10mb-100mb)
     str = str.replace(/ *\([^)]*\) */g, "");
+
     let r: any[];
     r = str.split("|");
     if (r.length < 5) {
