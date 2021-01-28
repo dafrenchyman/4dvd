f7c542b6eec7e667665908eb1950892071a7613a
Isaiah Dorado <isaiahdorado@gmail.com>
2021-01-28 10:38:02 -0800

    ID - Removes a duplicate function and fixes a reload bug

--- src/app/view.component.ts
+++ src/app/view.component.ts
@@ -730,14 +730,9 @@ export class ViewComponent implements OnInit, AfterViewInit {
     );
     this._controller.loadLevels(selectedDataset);
     this._controller.loadDataset(selectedDataset, selectedDataset.StartDate, 1);
-  }
-
-  public LoadSurfaceTemperature() {
-    const selectedDataset = this._model.settings.Datasets.find(myObj =>
-      myObj.FullName.includes("Global Surface Temperature|Monthly Mean")
-    );
-    this._controller.loadLevels(selectedDataset);
-    this._controller.loadDataset(selectedDataset, selectedDataset.StartDate, 1);
+    setTimeout(() => {
+      this.setSlider();
+    }, 700);
   }
 
   public OpenDataTreeDialog() {
