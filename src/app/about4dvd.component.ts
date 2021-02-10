5664c53c97dba97980c72541c683f05432274bbb
Isaiah Dorado <isaiahdorado@gmail.com>
2021-02-09 21:23:24 -0800

    prettier

--- src/app/about4dvd.component.ts
+++ src/app/about4dvd.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
