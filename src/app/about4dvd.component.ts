04d08180aa0a83c543cd3af34c65eb60be0243d4
Isaiah Dorado <isaiahdorado@gmail.com>
2021-02-24 14:23:02 -0800

    ID - Adding first half of interactive tutorial
    prettier
    
    ID - Adding the second half of the interactive tutorial
    
    styling changes

--- src/app/about4dvd.component.ts
+++ src/app/about4dvd.component.ts
@@ -1,33 +1,17 @@
-import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import { MatDialog } from "@angular/material/dialog";
+import { TutorialMenuComponent } from "./tutorial-menu/tutorial-menu.component";
 
 @Component({
   templateUrl: "./about4dvd.component.html",
   styleUrls: ["./about4dvd.component.css"]
 })
 export class About4dvdComponent implements OnInit {
-  safeURL;
-  videoURL = "https://www.youtube.com/watch?v=VFvWAFo4Lp8&t=1s";
-  constructor(private _sanitizer: DomSanitizer, private http: HttpClient) {
-    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(
-      this.videoURL
-    );
-  }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {}
 
-  // TODO: This is a temporary location for the tutorials.
-  //  When the interactive tutorial gets made, these tutorials will be offered alongside it and permanently moved there.
-  // gets the PDF version of the Basic Functions of 4DVD Tutorial
-  private async getPDF() {
-    try {
-      const res = await this.http
-        .get(`/assets/BasicFunctions4DVDTutorial.pdf`, { responseType: "blob" })
-        .toPromise();
-      window.open(window.URL.createObjectURL(res));
-    } catch (e) {
-      console.log(e);
-    }
+  private openTutorialDialog() {
+    const dialogRef = this.dialog.open(TutorialMenuComponent);
   }
 }
